326b8cabaf2f23c0d610d7044290fdf0204edc2c
Asdullah Siddique <asdullah.siddique@gmail.com>
2021-12-06 13:27:54 +0400

    implement fetch all

--- src/product/index.js
+++ src/product/index.js
@@ -1,3 +1,5 @@
+import { stringify as stringifyQuery } from 'query-string';
+
 /**
  *
  * @typedef {Object} ProductVariantInventoryLocation
@@ -65,3 +67,44 @@
 export default function ProductClient(client) {
   this.client = client;
 }
+
+/**
+ *
+ * @typedef {Object} FetchAllProductsResponse
+ * @property {Array<Product>} products
+ * @property {Object} connection
+ * @property {string} connection.platform
+ * @property {string} connection.id
+ * @property {string} request_id
+ * @property {string} next_cursor
+ */
+
+/**
+ * @typedef {Object} FetchAllProductsOptions
+ * @property {string} access_token
+ * @property {number} [limit]
+ * @property {string} [cursor]
+ * @property {string} [status]
+ * @property {string} [name]
+ * @property {number} [created_at_min]
+ * @property {number} [created_at_max]
+ * @property {Array<string>} [ids]
+ */
+
+/**
+ *
+ * @param {FetchAllProductsOptions} options
+ * @returns {FetchAllProductsResponse}
+ * @throws {Error}
+ * @throws {import("../RutterError").RutterError}
+ */
+ProductClient.prototype.fetchAll = async function fetchAll(options) {
+  const { data } = await this.client.request({
+    method: 'get',
+    url: `/products?${stringifyQuery(options, {
+      skipNull: true,
+      skipEmptyString: true,
+    })}`,
+  });
+  return data;
+};
