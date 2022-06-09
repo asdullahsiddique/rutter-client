8f8ca6a3fc20aad039a11ccf08a427fb306ae451
tvermote <t.vermote@udroppy.com>
2022-06-09 21:51:09 +0400

    fix typo

--- src/connection/index.js
+++ src/connection/index.js
@@ -159,7 +159,7 @@ ConnectionClient.prototype.create = async function create(options) {
  * @throws {Error}
  * @throws {import("../RutterError").RutterError}
  */
-ConnectionClient.prototype.remove = async function removeß({ connectionId }) {
+ConnectionClient.prototype.remove = async function remove({ connectionId }) {
   return this.client.request({
     method: 'delete',
     url: `/connections/${connectionId}`,
